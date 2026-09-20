973bfbd26c9a7b587c2ad9fed63018b534d08174
annie-ling <annie7837013@gmail.com>
2026-09-20 20:29:39 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? './' : '/',
-  build: { target: 'es2020' }
+  build: {
+    target: 'es2020',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html')
+      }
+    }
+  }
 }));
